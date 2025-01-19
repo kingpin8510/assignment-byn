d10a22c28caa4700f71811f949abfc4ddb1458cf
kingpin8510 <rp240479@gmail.com>
2025-01-20 05:20:30 +0530

    Updated mapview

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,28 +1,24 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useProfiles } from "../contexts/ProfileContext";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import React from "react"
+import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet"
+import { useProfiles } from "../contexts/ProfileContext"
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
 
-delete L.Icon.Default.prototype._getIconUrl;
+delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
   iconUrl: require("leaflet/dist/images/marker-icon.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
+})
 
 function MapView({ selectedProfile }) {
-  const { profiles } = useProfiles();
-  const centerPosition = [39.8283, -98.5795]; // Center of the US
-  const zoom = selectedProfile ? 13 : 4;
+  const { profiles } = useProfiles()
+  const centerPosition = [39.8283, -98.5795] // Center of the US
+  const zoom = selectedProfile ? 13 : 4
 
   return (
     <MapContainer
-      center={
-        selectedProfile
-          ? [selectedProfile.latitude, selectedProfile.longitude]
-          : centerPosition
-      }
+      center={selectedProfile ? [selectedProfile.latitude, selectedProfile.longitude] : centerPosition}
       zoom={zoom}
       style={{ height: "100%", width: "100%" }}
     >
@@ -30,10 +26,11 @@ function MapView({ selectedProfile }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {selectedProfile ? (
-        <Marker
-          position={[selectedProfile.latitude, selectedProfile.longitude]}
-        >
+      {selectedProfile && (
+        <Marker position={[selectedProfile.latitude, selectedProfile.longitude]}>
+          <Tooltip direction="top" offset={[0, -20]} opacity={1} permanent>
+            {selectedProfile.name}
+          </Tooltip>
           <Popup>
             <div>
               <h3 className="font-bold">{selectedProfile.name}</h3>
@@ -41,12 +38,13 @@ function MapView({ selectedProfile }) {
             </div>
           </Popup>
         </Marker>
-      ) : (
+      )}
+      {!selectedProfile &&
         profiles.map((profile) => (
-          <Marker
-            key={profile.id}
-            position={[profile.latitude, profile.longitude]}
-          >
+          <Marker key={profile.id} position={[profile.latitude, profile.longitude]}>
+            <Tooltip direction="top" offset={[0, -20]} opacity={1}>
+              {profile.name}
+            </Tooltip>
             <Popup>
               <div>
                 <h3 className="font-bold">{profile.name}</h3>
@@ -54,10 +52,10 @@ function MapView({ selectedProfile }) {
               </div>
             </Popup>
           </Marker>
-        ))
-      )}
+        ))}
     </MapContainer>
-  );
+  )
 }
 
-export default MapView;
+export default MapView
+
